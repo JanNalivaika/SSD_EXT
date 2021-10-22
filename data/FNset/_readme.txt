7893e5d2ccc64c5e7f509270d123dc301759307d
Jan Nalivaika <nalivaika@outlook.de>
2021-10-22 17:37:56 -0400

    removed folder data

--- data/FNset/_readme.txt
+++
@@ -1,2 +0,0 @@
-
- contains binvox-files which are used for generating of training and validation sets
--- data/config_medium/03_move_binvox.bat
+++
@@ -1,9 +0,0 @@
-
-
-REM move binvox-files from .\stls to ..\FNset\
-
-del ..\FnSet\*.binvox
-
-move .\stls_medium\*.binvox ..\FnSet\
-
-pause
