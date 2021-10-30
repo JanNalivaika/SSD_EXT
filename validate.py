61b1c28a0ec9abff32b2aad6489dca11a1abb843
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-10-30 21:21:47 +0200

    try to fix issue "TypeError: argument should be integer or bytes-like object, not 'str'"

--- validate.py
+++ validate.py
@@ -491,11 +491,11 @@ def create_weights():
     # This second link is only losely related, but echos the first, "processing a ZIP archive often requires backwards seeking"
     f = open(zipFile, 'r+b')
     content = f.read()
-    pos = content.rfind('\x50\x4b\x05\x06') # reverse find: this string of bytes is the end of the zip's central directory.
+    pos = content.rfind(b'\x50\x4b\x05\x06') # reverse find: this string of bytes is the end of the zip's central directory.
     if pos>0:
         f.seek(pos+20) # +20: see secion V.I in 'ZIP format' link above.
         f.truncate()
-        f.write('\x00\x00') # Zip file comment length: 0 byte length; tell zip applications to stop reading.
+        f.write(b'\x00\x00') # Zip file comment length: 0 byte length; tell zip applications to stop reading.
         f.seek(0)
         f.close()
 
