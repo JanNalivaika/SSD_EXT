f7a649b62c5f52df01e65292f59105d64a8f0d3b
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2021-11-17 19:09:17 +0100

    fix run-time error - fix config-size to the size of voc.pth

---
+++ unit_tests/test_validate.py
@@ -0,0 +1,17 @@
+#
+# test_validate.py
+#
+import unittest
+import os
+import validate
+
+
+# noinspection PyPep8Naming
+class test_validate(unittest.TestCase):
+    def test_run(self):
+        log = validate.run()
+        self.assertTrue("found feature 4.0 in picture data/MulSet/set20/90_5.png" in log)
+
+
+if __name__ == '__main__':
+    unittest.main()
